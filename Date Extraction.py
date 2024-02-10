9a890a7615f424e7110e5e232932819b753c5839
Kazim Hussain <77475849+Kazim-H@users.noreply.github.com>
2024-02-10 22:56:12 +0500

    Update Date Extraction.py

--- Date Extraction.py
+++ Date Extraction.py
@@ -1,4 +1,5 @@
 import re
 sample_string = "- Today is 2024-02-05. - Tomorrow's date is 05/02/2024. - The event will take place on 2024-02-10 at 10:00 AM. - Another date: 02-05-24. - Don't forget the meeting on 2024-02-06!"
-dates = re.findall('([0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{2}/[0-9]{2}/[0-9]{4}|[0-9]{1,2}-[0-9]{2}-[0-9]{2})', sample_string)
+pattern = '(?:\d{4}|\d{2})[-/]\d{2}[-/](?:\d{4}|\d{2})'
+dates = re.findall(pattern, sample_string)
 print(dates)
